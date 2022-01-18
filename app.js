77bdef12ac1860e7e3fd3b7b32f12ef377de8f35
aryanthesaini <68115113+aryanthesaini@users.noreply.github.com>
2022-01-19 00:06:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -74,6 +74,29 @@
 //     }
 // });
 
+
+
+let mouseCursor = document.querySelector('.cursor');
+let navLinks = document.querySelectorAll('a')
+window.addEventListener('mousemove', cursor);
+function cursor(e) {
+    mouseCursor.style.top = e.clientY + 'px';
+    mouseCursor.style.left = e.clientX + 'px';
+}
+
+navLinks.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        mouseCursor.classList.add('link-grow');
+        link.classList.add('hovered-link');
+    })
+    link.addEventListener('mouseleave', () => {
+        mouseCursor.classList.remove('link-grow');
+        link.classList.remove('hovered-link');
+    })
+})
+
+
+
 let tl = gsap.timeline({
     scrollTrigger: {
         trigger: ".second-page",
@@ -88,5 +111,6 @@ tl.fromTo('.second-page', { clipPath: 'circle(4%)' }, { clipPath: 'circle(75%)',
 tl.fromTo('.code-note', { scale: 0.5 }, { scale: 0, opacity: 0, duration: 1 }, '-=3');
 tl.fromTo('.title', { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo('.subtitle', { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo('.third-page', { clipPath: 'circle(4%)' }, { clipPath: 'circle(75%)', duration: 1 });
 // tl.fromTo('.custom', { opacity: 0 }, { opacity: 1, duration: 1 });
 
