6bc1d20af7f74f1b57b31b3bf31761cf7cef801e
aryanthesaini <68115113+aryanthesaini@users.noreply.github.com>
2022-01-20 15:33:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -75,6 +75,22 @@
 // });
 
 
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+        invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+        el: '.blog-slider__pagination',
+        clickable: true,
+    }
+});
+
+
+
 
 let mouseCursor = document.querySelector('.cursor');
 let navLinks = document.querySelectorAll('a')
